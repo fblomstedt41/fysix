53eeeb8d2a943199a5d77732e6a613d038f79161
Fredrik Blomstedt <98818218+fblomstedt41@users.noreply.github.com>
2025-07-24 01:19:40 +0200

    Add stubs for missing dependencies and update ignore

---
+++ src/main/java/net/force2dev/fysix/sound/PlaySound.java
@@ -0,0 +1,7 @@
+package net.force2dev.fysix.sound;
+
+public class PlaySound {
+    public static void thrust() {
+        // Stub method for playing thrust sound
+    }
+}
---
+++ src/main/java/net/sf/janalogtv/AnalogTV.java
@@ -0,0 +1,13 @@
+package net.sf.janalogtv;
+
+import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
+
+public class AnalogTV {
+    public AnalogTV(int width, int height, GraphicsConfiguration gc) {}
+    public void setBrightnessControl(double value) {}
+    public void setFlutterHorizDesync(boolean value) {}
+    public void initSignal(double value) {}
+    public void addSignal(Reception r) {}
+    public void draw(Graphics g) {}
+}
